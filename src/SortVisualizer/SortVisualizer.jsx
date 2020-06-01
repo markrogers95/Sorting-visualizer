459999c9ec1d56ae0e678a70ac7536877482fef6
markrogers95 <mark_199518@hotmail.co.uk>
2020-06-01 21:27:26 +0100

    App start

---
+++ src/SortVisualizer/SortVisualizer.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import "./SortVisualizer.css";
+
+export default class SortVisualizer extends React.Component {
+
+    constructor(props){
+        
+        super(props);
+
+        this.state = { array: [], };
+
+    }
+
+    componentDidMount() {
+
+        this.resetArray();
+    }
+
+    resetArray() {
+
+        const array = [];
+
+        for (var i = 0; i < 200; i++){
+
+            array.push(randInt(25, window.innerHeight - 100));
+
+        }
+
+        this.setState({array});
+    }
+
+    render() {
+
+        const {array} = this.state;
+
+        return (
+            
+            <div className="wrap">
+                {array.map((value, idx) => (
+                    <div className="array" key={idx} style={{height: `${value}px`}}></div>
+            ))}
+            </div> 
+        );
+    }
+}
+
+function randInt(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1) - min);
+
+}
